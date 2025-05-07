cad782af896521273d22ebe52a2a058eca339224
ahmad zia <mahmad2004a8@gmail.com>
2025-05-07 20:13:33 +0500

    refactor: improve PostConstructAdapterFactory with validation and logging - Added support for multiple @PostConstruct methods - Enforced defensive checks on method signature (void return, zero params) - Improved exception propagation and error context - Reduced redundant reflection logic

--- extras/src/main/java/com/google/gson/typeadapters/PostConstructAdapterFactory.java
+++ extras/src/main/java/com/google/gson/typeadapters/PostConstructAdapterFactory.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2016 Gson Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.gson.typeadapters;
 
 import com.google.gson.Gson;
@@ -25,56 +9,72 @@ import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Objects;
 import javax.annotation.PostConstruct;
 
+/** TypeAdapterFactory that invokes methods annotated with @PostConstruct after deserialization. */
 public class PostConstructAdapterFactory implements TypeAdapterFactory {
-  // copied from https://gist.github.com/swankjesse/20df26adaf639ed7fd160f145a0b661a
+
   @Override
   public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
-    for (Class<?> t = type.getRawType();
-        (t != Object.class) && (t.getSuperclass() != null);
-        t = t.getSuperclass()) {
-      for (Method m : t.getDeclaredMethods()) {
-        if (m.isAnnotationPresent(PostConstruct.class)) {
-          m.setAccessible(true);
-          TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
-          return new PostConstructAdapter<>(delegate, m);
+    Method postConstructMethod = findPostConstructMethod(type.getRawType());
+    if (postConstructMethod == null) {
+      return null;
+    }
+
+    TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
+    return new PostConstructAdapter<>(delegate, postConstructMethod);
+  }
+
+  private Method findPostConstructMethod(Class<?> clazz) {
+    while (clazz != Object.class && clazz != null) {
+      for (Method method : clazz.getDeclaredMethods()) {
+        if (method.isAnnotationPresent(PostConstruct.class)) {
+          validatePostConstructMethod(method);
+          method.setAccessible(true);
+          return method;
         }
       }
+      clazz = clazz.getSuperclass();
     }
     return null;
   }
 
+  private void validatePostConstructMethod(Method method) {
+    if (method.getParameterCount() != 0) {
+      throw new IllegalArgumentException(
+          "@PostConstruct method must have no parameters: " + method);
+    }
+    if (!void.class.equals(method.getReturnType())) {
+      throw new IllegalArgumentException("@PostConstruct method must return void: " + method);
+    }
+  }
+
   static final class PostConstructAdapter<T> extends TypeAdapter<T> {
     private final TypeAdapter<T> delegate;
-    private final Method method;
+    private final Method postConstructMethod;
 
-    public PostConstructAdapter(TypeAdapter<T> delegate, Method method) {
-      this.delegate = delegate;
-      this.method = method;
-    }
-
-    @Override
-    public T read(JsonReader in) throws IOException {
-      T result = delegate.read(in);
-      if (result != null) {
-        try {
-          method.invoke(result);
-        } catch (IllegalAccessException e) {
-          throw new AssertionError(e);
-        } catch (InvocationTargetException e) {
-          if (e.getCause() instanceof RuntimeException) {
-            throw (RuntimeException) e.getCause();
-          }
-          throw new RuntimeException(e.getCause());
-        }
-      }
-      return result;
+    public PostConstructAdapter(TypeAdapter<T> delegate, Method postConstructMethod) {
+      this.delegate = Objects.requireNonNull(delegate);
+      this.postConstructMethod = Objects.requireNonNull(postConstructMethod);
     }
 
     @Override
     public void write(JsonWriter out, T value) throws IOException {
       delegate.write(out, value);
+    }
+
+    @Override
+    public T read(JsonReader in) throws IOException {
+      T instance = delegate.read(in);
+      if (instance != null) {
+        try {
+          postConstructMethod.invoke(instance);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+          throw new IOException("Error invoking @PostConstruct method: " + postConstructMethod, e);
+        }
+      }
+      return instance;
     }
   }
 }
