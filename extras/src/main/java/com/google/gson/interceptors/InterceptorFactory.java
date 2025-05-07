5dfdad610ec06bf7e1eaa578d1dbaa65b439331a
ahmad zia <mahmad2004a8@gmail.com>
2025-05-07 20:10:16 +0500

    - Replaced broad catch blocks with ReflectiveOperationException - Added null checks post-deserialization - Restricted raw type suppression and clarified intent

--- extras/src/main/java/com/google/gson/interceptors/InterceptorFactory.java
+++ extras/src/main/java/com/google/gson/interceptors/InterceptorFactory.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2012 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.gson.interceptors;
 
 import com.google.gson.Gson;
@@ -24,8 +8,12 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 
-/** A type adapter factory that implements {@code @Intercept}. */
+/**
+ * A type adapter factory that implements {@code @Intercept} by wrapping deserialized objects with a
+ * post-deserialization interceptor.
+ */
 public final class InterceptorFactory implements TypeAdapterFactory {
+
   @Override
   public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
     Intercept intercept = type.getRawType().getAnnotation(Intercept.class);
@@ -37,17 +25,21 @@ public final class InterceptorFactory implements TypeAdapterFactory {
     return new InterceptorAdapter<>(delegate, intercept);
   }
 
-  static class InterceptorAdapter<T> extends TypeAdapter<T> {
+  private static class InterceptorAdapter<T> extends TypeAdapter<T> {
     private final TypeAdapter<T> delegate;
     private final JsonPostDeserializer<T> postDeserializer;
 
-    @SuppressWarnings("unchecked") // ?
+    @SuppressWarnings("unchecked")
     public InterceptorAdapter(TypeAdapter<T> delegate, Intercept intercept) {
+      this.delegate = delegate;
       try {
-        this.delegate = delegate;
-        this.postDeserializer = intercept.postDeserialize().getDeclaredConstructor().newInstance();
-      } catch (Exception e) {
-        throw new RuntimeException(e);
+        this.postDeserializer =
+            (JsonPostDeserializer<T>)
+                intercept.postDeserialize().getDeclaredConstructor().newInstance();
+
+      } catch (ReflectiveOperationException e) {
+        throw new IllegalStateException(
+            "Failed to instantiate post deserializer: " + intercept.postDeserialize().getName(), e);
       }
     }
 
@@ -59,7 +51,9 @@ public final class InterceptorFactory implements TypeAdapterFactory {
     @Override
     public T read(JsonReader in) throws IOException {
       T result = delegate.read(in);
-      postDeserializer.postDeserialize(result);
+      if (result != null) {
+        postDeserializer.postDeserialize(result);
+      }
       return result;
     }
   }
